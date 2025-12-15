43f656b07dbe777ee784023eef9dfc711a8cc467
M.Nagaruthwik <nagaruthwikmerugu162@gmail.com>
2025-12-15 15:50:35 +0530

    cvb

--- app/layout.js
+++ app/layout.js
@@ -5,16 +5,6 @@ import { Analytics } from "@vercel/analytics/next"
 import Preloader from "./Components/Preloader";
 
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
   title: "Nothing Real",
   description: "We turn your NOTHING complexity vision into REAL revolutionary ideas feels inevitable",
@@ -24,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
         <Preloader/>
         <Navbar />
